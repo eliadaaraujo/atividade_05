a6b62c1e329361936f1d98f63feeaba8e34703e2
eli <eliadadearaujolinhares@gmail.com>
2025-09-24 20:15:53 -0300

    Add files via upload

---
+++ atividade05.py
@@ -0,0 +1,7 @@
+# Atividade 5 – Concatenação de Strings
+# Crie duas variáveis: nome e sobrenome.
+# Mostre o nome completo juntando as duas.
+
+nome = "Eliada"
+sobrenome = "de Araujo Linhares"
+print(f'{nome} {sobrenome}')
